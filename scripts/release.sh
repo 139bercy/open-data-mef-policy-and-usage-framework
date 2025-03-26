6f179a6d92683c4c2aebe80afd71cb1803137290
tongyeouki <tongyeouki@gmail.com>
2025-03-26 09:58:47 +0100

    refactor: refactor release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,39 +8,46 @@ TITLE=$(head -n 1 $FILE | sed 's/^# //' )
 
 sed '1,/^\s*## Introduction\s*$/ { /^\s*## Introduction\s*$/!d; }' $FILE > temp.md
 
-echo Export content to "$FILENAME.docx"
+export_pdf () {
+  echo Export content to "$FILENAME.pdf"
+  pandoc -o "build/$FILENAME".pdf -t pdf -f gfm temp.md \
+    --pdf-engine=lualatex \
+    --from markdown-implicit_figures \
+    --template scripts/templates/default.tex \
+    --metadata TITLE="$TITLE" \
+    --metadata VERSION="$VERSION" \
+    --resource-path=.:src/media/
+}
 
-pandoc -o "build/$FILENAME".docx -t docx -f gfm $FILE \
-  --from markdown-yaml_metadata_block \
-  --css=style.css \
-  --standalone \
-  --resource-path=.:src:media:./src/media/
+export_tex () {
+  echo Export content to "$FILENAME.tex"
+  pandoc -o "build/$FILENAME".tex -t latex -f gfm temp.md \
+    --from markdown-implicit_figures \
+    --template scripts/templates/default.tex \
+    --metadata TITLE="$TITLE" \
+    --metadata VERSION="$VERSION" \
+    --resource-path=.:./src/media/
+}
 
+export_docx () {
+  echo Export content to "$FILENAME.docx"
+  pandoc -o "build/$FILENAME".docx -t docx -f gfm $FILE \
+    --from markdown-yaml_metadata_block \
+    --css=style.css \
+    --standalone \
+    --resource-path=.:src:media:./src/media/
+}
 
-echo Export content to "$FILENAME.odt"
+export_odt () {
+  echo Export content to "$FILENAME.odt"
+  pandoc "$FILE" --reference-doc=scripts/templates/default.ott -o "build/$FILENAME".odt \
+    --metadata version="$VERSION" \
+    --resource-path=.:src:media:./src/media/
+}
 
-pandoc "$FILE" --reference-doc=scripts/templates/default.ott -o "build/$FILENAME".odt \
-  --metadata version="$VERSION" \
-  --resource-path=.:src:media:./src/media/
+export_odt
+export_pdf
 
-
-echo Export content to "$FILENAME.tex"
-
-pandoc -o "build/$FILENAME".tex -t latex -f gfm temp.md \
---from markdown-implicit_figures \
---template scripts/templates/default.tex \
---metadata TITLE="$TITLE" \
---metadata VERSION="$VERSION" \
---resource-path=.:./src/media/
-
-echo Export content to "$FILENAME.pdf"
-
-
-pandoc -o "build/$FILENAME".pdf -t pdf -f gfm temp.md \
-  --pdf-engine=lualatex \
-  --from markdown-implicit_figures \
-  --template scripts/templates/default.tex \
-  --metadata TITLE="$TITLE" \
-  --metadata VERSION="$VERSION" \
-  --resource-path=.:src/media/
 rm temp.md
+
+echo "Success!"
